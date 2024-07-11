0dfde26823fdb60ed310c2dfd4fc6fb7fb60103f
Aravind <69782788+AravindLingoju@users.noreply.github.com>
2024-07-12 01:27:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Loard Krishna</h1>
+     < img src={require("./images/krishna.jpg")} alt="images" style={{width:"3q0%", height:"20%"}}/>
+   
+     <p>Krishna is a major Hindu god. Often called Lord Krishna, he is one of the most widely worshiped and popular Hindu deities. Krishna is the eighth avatar (or incarnation) of Vishnu. He was deified in the 5th century and since then has also been worshiped as the supreme god himself. Krishna is the Hindu god of compassion, protection, and love.
+
+Also called the Dark One, Krishna is an important character in many Hindu sacred texts, including the Bhagavad Gita. He is portrayed in various ways in Indian mythology and art, from child-god to divine lover, prankster to hero. However, there are three main stages of incarnation within the story of Krishna: the baby, the lover, and the sage. These three stages are believed to symbolize key aspects of his significance and power within Hindu mythology.
+
+Krishna is known by 108 different names and emanations, many of which reflect his attributes and mythological associations. The name Krishna itself means black or dark blue. Some of his more popular epithets are Mohan, the enchanter, Govinda, the chief herdsman, Keev, the prankster. Some epithets and names are used only in specific geographical locations. One example is Jagannatha, a name popular in the Odisha state region of India. The honorary title Sri is often placed before the name Krishna, meaning Lord.</p>
+    </div>
+  );
+}
+
+export default App;
